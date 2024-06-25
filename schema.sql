2453d0458b7847dbaffe691215d934ace650ad98
Ankit Kumar Prem <108314952+codexankit@users.noreply.github.com>
2024-06-25 16:30:00 +0530

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE studentsa (
+  id int8 PRIMARY KEY AUTO_INCREMENT,
+  firstName VARCHAR(255) NOT NULL,
+  lastName VARCHAR(255) NOT NULL,
+  stipend INT NOT NULL
+);
+
+
+
+-- data.sql is getting used in the tests or not, I think its not.
+-- why do we need an external database when we have this data.sql here
+-- changes in the application.properties
+-- what is this application.sql
+-- get these .sql thing fixed
+
+-- deployment stuffs
+--
+
+
