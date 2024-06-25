7b776f4bee2edbfaa10730d887a5223963b24e81
Ankit Kumar Prem <108314952+codexankit@users.noreply.github.com>
2024-06-25 16:29:31 +0530

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,22 @@
+INSERT INTO studentsa
+(firstName, lastName, stipend)
+VALUES ("Ankit", "Prem", 550000)
+
+INSERT INTO studentsa
+(firstName, lastName, stipend)
+VALUES ("Wtf", "Jack", 550000)
+
+INSERT INTO studentsa
+(firstName, lastName, stipend)
+VALUES ("Damini", "Kalthiya", 5500)
+
+INSERT INTO studentsa
+(firstName, lastName, stipend)
+VALUES ("Doja", "Verma", 100000)
+
+--
+
+
+
+
+
